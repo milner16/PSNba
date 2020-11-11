fbeec377c6a3cddd357b0cb0461a3fe1c812925a
milner16 <alex.milner16@gmail.com>
2020-11-11 02:30:08 -0700

    Fix Test in CI/CD config

--- Tests/Tests.ps1
+++ Tests/Tests.ps1
@@ -1,3 +1,4 @@
+Import-Module -Name "$($PSScriptRoot)\..\PSNba" -Force
 Describe "Get-NbaTeam"{
     It "outputs 'Utah Jazz'"{
         (Get-NbaTeam -TeamId "1610612762" -Year 2019).fullName | Should Be 'Utah Jazz'

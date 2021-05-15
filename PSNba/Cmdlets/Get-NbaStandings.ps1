ae66fcfcf59505ad55f6f4ab3170696a1d29b2bc
milner16 <alex.milner16@gmail.com>
2021-05-14 18:25:02 -0600

    Fix Standings Endpoint

--- PSNba/Cmdlets/Get-NbaStandings.ps1
+++ PSNba/Cmdlets/Get-NbaStandings.ps1
@@ -1,16 +1,17 @@
 function Get-NbaStandings {
     [CmdletBinding()]
     param (
-        # Date
-        [Parameter(Mandatory = $false)]
-        [datetime]
-        $Date,
-
         # Standings Type
         [Parameter(Mandatory = $false)]
         [ValidateSet("All", "Conference", "Division")]
         [string]
-        $Type = "All"
+        $Type = "All",
+
+        # Date
+        [Parameter(Mandatory = $false)]
+        [Obsolete('-Date is not currently supported. Current Standings will be returned instead.')]
+        [datetime]
+        $Date
     )
     
     begin {
@@ -18,14 +19,7 @@ function Get-NbaStandings {
     }
     
     process {
-        if ($Date) {
-            [string] $dateStr = ConvertTo-DateString -Date $Date
-        }
-        else {
-            $dateStr = "current"
-        }
-        [string] $endpoint = $Script:Endpoints.Standings.Replace("{date}", $dateStr)
-        $endpoint = $endpoint.Replace("{type}", $Type.ToLower())
+        [string] $Endpoint = $Script:Endpoints.Standings.Replace("{type}", $Type.ToLower())
         $response = Invoke-NbaRequest -Uri $endpoint -Method:Get
         return $response.league.standard
     }
--- PSNba/Configuration/Endpoints.psd1
+++ PSNba/Configuration/Endpoints.psd1
@@ -11,7 +11,7 @@
     RecapArticle      = "http://data.nba.com/data/10s/prod/v1/{date}/{gameId}_recap_article.json"
     Schedule          = "http://data.nba.com/data/10s/prod/v1/{season}/schedule.json"
     Scoreboard        = "http://data.nba.com/data/5s/json/cms/noseason/scoreboard/{date}/games.json"
-    Standings         = "http://data.nba.net/data/10s/prod/v1/{date}/standings_{type}.json"
+    Standings         = "http://data.nba.net/data/10s/prod/v1/current/standings_{type}.json"
     TeamLeaders       = "http://data.nba.com/data/10s/prod/v1/{season}/teams/{teamId}/leaders.json"
     Teams             = "http://data.nba.net/data/10s/prod/v1/{year}/teams.json"
     TeamSchedule      = "http://data.nba.com/data/10s/prod/v1/{season}/teams/{teamId}/schedule.json"

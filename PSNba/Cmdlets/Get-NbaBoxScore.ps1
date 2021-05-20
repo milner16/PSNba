7f1920144c5d8fe26b3cdbe2186d0ffeaa2f43fb
milner16 <alex.milner16@gmail.com>
2021-05-20 01:05:22 -0600

    Add Output Formatting

--- PSNba/Cmdlets/Get-NbaBoxScore.ps1
+++ PSNba/Cmdlets/Get-NbaBoxScore.ps1
@@ -14,9 +14,20 @@ function Get-NbaBoxScore {
             Mandatory = $true, 
             ValueFromPipelineByPropertyName = $true
         )]
-        [Alias("homeStartDate")]
+        [Alias("homeStartDate", "startDateEastern")]
         [string]
-        $Date
+        $Date,
+
+        # Type
+        [Parameter(Mandatory = $false)]
+        [ValidateSet('Team', 'Player')]
+        [string]
+        $Type = 'Team',
+
+        # Returns the raw json
+        [Parameter(Mandatory = $false)]
+        [switch]
+        $Raw
     )
     
     begin {
@@ -24,10 +35,42 @@ function Get-NbaBoxScore {
     }
     
     process {
-        [string] $endpoint = $Script:Config.Endpoints.BoxScore.Replace("{gameId}", $GameId)
-        $endpoint = $endpoint.Replace("{date}", $Date) 
-        $response = Invoke-NbaRequest -Uri $endpoint -Method:Get
-        return $response.sports_content
+        Write-Verbose -Message "Type = $($Type)"
+        [string] $Endpoint = $Script:Config.Endpoints.BoxScore.Replace("{gameId}", $GameId)
+        $Endpoint = $Endpoint.Replace("{date}", $Date) 
+        $Response = Invoke-NbaRequest -Uri $Endpoint -Method:Get
+
+        if ($Raw) {
+            return $Response
+        }
+
+        [string[]] $TeamTypes = @("visitor", "home")
+        
+        if ($Type -eq 'Team') {
+            foreach ($TeamType in $TeamTypes) {
+                $Team = $Response.sports_content.game.$($TeamType) 
+                $DateTime = ConvertFrom-DateTimeString -DateString $Response.sports_content.game.date -TimeString $Response.sports_content.game.time
+                $BoxScore = [NbaBoxScorePeriod]::new()
+                $BoxScore.GameId = $GameId
+                $BoxScore.Date = $DateTime
+                $BoxScore.TeamName = $Team.abbreviation
+                foreach ($LineScore in $Team.linescores.period) {
+                    $BoxScore.Periods.Add($LineScore.period_name, $LineScore.score)
+                    $BoxScore.TotalScore += $LineScore.score
+                }
+                $BoxScore
+            }
+        }
+
+        if ($Type -eq 'Player') {
+            foreach ($TeamType in $TeamTypes) {
+                $Team = $Response.sports_content.game.$($TeamType) 
+                $Players = $Response.sports_content.game.$($TeamType).players.player
+                foreach ($Player in $Players) {
+                    [NbaBoxScorePlayer]::new($Player, $Team.abbreviation)
+                }
+            }
+        }
     }
     
     end {
